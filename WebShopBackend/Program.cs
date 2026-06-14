d382498ef22abe6d1c879b11d54c051111e01ec4
Kekse4life <danielfida08@gmail.com>
2026-06-14 18:20:23 +0200

    Refine CORS policy to specify allowed origins and enhance security

--- WebShopBackend/Program.cs
+++ WebShopBackend/Program.cs
@@ -32,7 +32,6 @@ builder.Services.AddCors(options =>
         .WithMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .WithHeaders("Content-Type", "Authorization")
         .AllowCredentials();
-        .AccessControlAllowOrigin("https://wineandoil.shop")
     });
 });
 
