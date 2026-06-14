ac58f9105ff2bde70a58042a15bf2d3ecea4d38b
Kekse4life <danielfida08@gmail.com>
2026-06-14 18:12:57 +0200

    Refine CORS policy to include Access-Control-Allow-Origin for frontend domain

--- WebShopBackend/Program.cs
+++ WebShopBackend/Program.cs
@@ -31,7 +31,8 @@ builder.Services.AddCors(options =>
         )
         .WithMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .WithHeaders("Content-Type", "Authorization")
-        .AllowCredentials();
+        .AllowCredentials();^
+        .AccessControlAllowOrigin("https://wineandoil.shop")
     });
 });
 
